06750791e680a3f34278ea8266104e2da606809c
amondocorre <amondocorre@gmail.com>
2026-09-09 21:13:53 -0400

    actualiza vista gerencial

--- application/config/routes.php
+++ application/config/routes.php
@@ -52,3 +52,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'welcome';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+
+// Mapeo de rutas para Control Métricas (Compatibilidad Linux/Apache)
+$route['controlmetricas/(:any)'] = 'ControlMetricas/$1';
+$route['controlmetricas'] = 'ControlMetricas';
+$route['control-metricas/(:any)'] = 'ControlMetricas/$1';
+$route['control-metricas'] = 'ControlMetricas';
+$route['ControlMetricas/(:any)'] = 'ControlMetricas/$1';
+$route['ControlMetricas'] = 'ControlMetricas';
